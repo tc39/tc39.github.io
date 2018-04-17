bfc94b868009df6bba4c799c02a9c12a19214140
Iddan Aharonson <mail@iddan.co>
2018-04-18 02:39:26 +0300

    removed header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
-import Header from '../components/header'
 import PageMenu from '../components/PageMenu'
 import './index.css'
 
@@ -15,23 +13,14 @@ const Layout = ({children, data}) => (
         {name: 'keywords', content: 'sample, something'},
         {name: 'charset', content: 'UTF-8"'},
         {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-        {name: 'theme-color', content: '#157878'},
+        {name: 'theme-color', content: '#FA7C23'},
       ]}
       htmlAttributes={{
         lang: data.site.siteMetadata.lang,
       }}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
     <PageMenu />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}>
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
