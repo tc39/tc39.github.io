6b6ccd2d40669c10bbb77123a7cced6d65d4c2b8
Christian Stuff <christian.stuff@namics.com>
2018-05-24 09:15:03 +0200

    Issue #12 - Update styles. Add transition to featurelist. Add prettier & stylelint-order.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,15 @@
  */
 function start() {
   var items = document.querySelectorAll('.featurelist__item .featurelist__item__intro');
-  items.forEach(function(v) {
-    v.addEventListener('click', function(evt) {
-      console.log(this);
-      this.parentNode.classList.toggle('open');
-    })
-  })
 
+  items.forEach(function (v) {
+    v.addEventListener('click', function (evt) {
+      var content = this.parentNode.querySelector('.featurelist__item__info');
+      var maxHeight = content.style.maxHeight;
+      content.style.maxHeight = Boolean(maxHeight) ? '' : content.scrollHeight + 'px';
+      this.parentNode.classList.toggle('open');
+    });
+  });
 }
 
 start();
