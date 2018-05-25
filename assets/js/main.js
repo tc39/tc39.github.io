d84b54278fccf077c28fc2083d96987400d8aaa1
Christian Stuff <christian.stuff@namics.com>
2018-05-25 13:47:27 +0200

    Issue #12 - Improve keyboard navigation for proposals

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,19 @@ function toggleMenu() {
   menu.parentNode.classList.toggle('open');
 }
 
+function toggleProposal(item) {
+  var content = item.querySelector('.featurelist__item__info');
+  var maxHeight = content.style.maxHeight;
+  content.style.maxHeight = Boolean(maxHeight) ? '' : content.scrollHeight + 'px';
+  content.setAttribute('aria-hidden', Boolean(maxHeight));
+  if (Boolean(maxHeight)) {
+    content.setAttribute('tabindex', '-1');
+  } else {
+    content.removeAttribute('tabindex');
+  }
+  item.classList.toggle('open');
+}
+
 /**
  * Represents the start of this application
  */
@@ -13,11 +26,27 @@ function start() {
 
   items.forEach(function (v) {
     v.addEventListener('click', function (ev) {
-      var content = this.parentNode.querySelector('.featurelist__item__info');
-      var maxHeight = content.style.maxHeight;
-      content.style.maxHeight = Boolean(maxHeight) ? '' : content.scrollHeight + 'px';
-      this.parentNode.classList.toggle('open');
+      toggleProposal(this.parentNode);
     });
+    v.addEventListener('keypress', function (ev) {
+      if (ev.key === "Enter" || ev.key === " ") {
+        ev.preventDefault();
+        toggleProposal(this.parentNode);
+      }
+    });
+    v.addEventListener('keydown', function (ev) {
+      var nextItem = ev.target.parentNode.nextElementSibling;
+      var prevItem = ev.target.parentNode.previousElementSibling;
+      if (ev.key === "ArrowDown" && nextItem && nextItem.classList.contains('featurelist__item')) {
+        ev.preventDefault();
+        nextItem.firstElementChild.focus();
+      }
+      if (ev.key === "ArrowUp" && prevItem && prevItem.classList.contains('featurelist__item')) {
+        ev.preventDefault();
+        prevItem.firstElementChild.focus();
+      }
+    });
+
   });
 
   document.querySelector('.menu-toggle').addEventListener('click', function (ev) {
