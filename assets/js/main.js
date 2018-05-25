ae690a9acaa1e323534a930f9883400be1a3758d
Christian Stuff <christian.stuff@namics.com>
2018-05-25 13:00:30 +0200

    Issue #12 - Implemented menu toggle. Optimized mobile view layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+  var menu = document.querySelector('#menu');
+  var maxHeight = menu.style.maxHeight;
+  menu.style.maxHeight = Boolean(maxHeight) ? '' : menu.scrollHeight + 'px';
+  menu.parentNode.classList.toggle('open');
+}
+
 /**
  * Represents the start of this application
  */
@@ -5,11 +12,28 @@ function start() {
   var items = document.querySelectorAll('.featurelist__item .featurelist__item__intro');
 
   items.forEach(function (v) {
-    v.addEventListener('click', function (evt) {
+    v.addEventListener('click', function (ev) {
       var content = this.parentNode.querySelector('.featurelist__item__info');
       var maxHeight = content.style.maxHeight;
       content.style.maxHeight = Boolean(maxHeight) ? '' : content.scrollHeight + 'px';
       this.parentNode.classList.toggle('open');
+    });
+  });
+
+  document.querySelector('.menu-toggle').addEventListener('click', function (ev) {
+    ev.preventDefault();
+    toggleMenu();
+  });
+
+  document.querySelectorAll('.menu-link').forEach(function (link) {
+    link.addEventListener('click', function (ev) {
+      ev.preventDefault();
+      toggleMenu();
+      var target = document.querySelector(ev.target.getAttribute('href'));
+      window.scrollTo({
+        behavior: 'smooth',
+        top: target.getBoundingClientRect().top
+      });
     });
   });
 }
