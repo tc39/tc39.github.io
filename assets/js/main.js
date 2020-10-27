d7dbeb8a7651f3e2f7543fcc8c56997e5f5c118b
Derek Lewis <DerekNonGeneric@inf.is>
2020-10-27 08:07:05 -0400

    🏗️✨ introduce ESLint (#214)
    * 📦 install array of dependencies used by ESLint
    
    * 🏗️ add minimal .eslintrc
    
    * 🏗️ add defaults to .browserlistrc
    
    * 🏗️ add _site to .eslintignore
    
    * 🏗️ add Babel configs
    
    * 🏗️ exclude babel.config.json
    
    * 🏗️✨ update lint:js & add format:js run scripts
    
    * 🏗️ add minimal .prettierrc
    
    * 🔧 appease ESLint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,37 @@
-"use strict";
+'use strict';
 
 function openMenu(menu) {
-  menu.style.maxHeight = menu.scrollHeight + "px";
+  menu.style.maxHeight = menu.scrollHeight + 'px';
   setTimeout(function() {
-    menu.style.maxHeight = "none";
+    menu.style.maxHeight = 'none';
   }, 600);
 }
 
 function closeMenu(menu) {
-  menu.style.maxHeight = menu.scrollHeight + "px";
+  menu.style.maxHeight = menu.scrollHeight + 'px';
   setTimeout(function() {
-    menu.style.maxHeight = "";
+    menu.style.maxHeight = '';
   }, 0);
 }
 
 function toggleMenu(menu) {
-  menu = menu || document.querySelector("#menu");
+  menu = menu || document.querySelector('#menu');
   var maxHeight = menu.style.maxHeight;
-  !!maxHeight ? closeMenu(menu) : openMenu(menu);
-  menu.classList.toggle("open");
+  maxHeight ? closeMenu(menu) : openMenu(menu);
+  menu.classList.toggle('open');
 }
 
 function toggleProposal(item) {
-  var content = item.querySelector(".featurelist__item__info");
+  var content = item.querySelector('.featurelist__item__info');
   var maxHeight = content.style.maxHeight;
-  content.style.maxHeight = !!maxHeight ? "" : content.scrollHeight + "px";
-  content.setAttribute("aria-hidden", !!maxHeight);
-  if (!!maxHeight) {
-    content.setAttribute("tabindex", "-1");
+  content.style.maxHeight = maxHeight ? '' : content.scrollHeight + 'px';
+  content.setAttribute('aria-hidden', !!maxHeight);
+  if (maxHeight) {
+    content.setAttribute('tabindex', '-1');
   } else {
-    content.removeAttribute("tabindex");
+    content.removeAttribute('tabindex');
   }
-  item.classList.toggle("open");
+  item.classList.toggle('open');
 }
 
 /**
@@ -39,17 +39,17 @@ function toggleProposal(item) {
  */
 function start() {
   var items = document.querySelectorAll(
-    ".featurelist__item .featurelist__item__example"
+    '.featurelist__item .featurelist__item__example'
   );
 
-  document.body.classList.remove("no-js");
+  document.body.classList.remove('no-js');
 
   items.forEach(function(v) {
-    v.addEventListener("click", function(ev) {
+    v.addEventListener('click', function() {
       toggleProposal(this.parentNode);
     });
-    v.addEventListener("keypress", function(ev) {
-      if (ev.key === "Enter" || ev.key === " ") {
+    v.addEventListener('keypress', function(ev) {
+      if (ev.key === 'Enter' || ev.key === ' ') {
         ev.preventDefault();
         toggleProposal(this.parentNode);
       }
@@ -57,30 +57,30 @@ function start() {
   });
 
   document
-    .querySelector(".menu-toggle")
-    .addEventListener("click", function(ev) {
+    .querySelector('.menu-toggle')
+    .addEventListener('click', function(ev) {
       ev.preventDefault();
       toggleMenu();
     });
 
-  document.querySelectorAll(".menu-link").forEach(function(link) {
-    var submenu = link.parentNode.querySelector(".submenu");
+  document.querySelectorAll('.menu-link').forEach(function(link) {
+    var submenu = link.parentNode.querySelector('.submenu');
     if (submenu) {
-      link.addEventListener("click", function(ev) {
-        var t = link.parentNode.querySelector(".submenu-toggle");
+      link.addEventListener('click', function(ev) {
+        var t = link.parentNode.querySelector('.submenu-toggle');
         ev.preventDefault();
         toggleMenu(submenu);
-        t.classList.toggle("open");
+        t.classList.toggle('open');
       });
     }
   });
 
-  document.querySelectorAll(".submenu-toggle").forEach(function(toggle) {
-    toggle.addEventListener("click", function(ev) {
+  document.querySelectorAll('.submenu-toggle').forEach(function(toggle) {
+    toggle.addEventListener('click', function(ev) {
       var t = ev.target;
-      var submenu = t.parentNode.querySelector(".submenu");
+      var submenu = t.parentNode.querySelector('.submenu');
       toggleMenu(submenu);
-      t.classList.toggle("open");
+      t.classList.toggle('open');
     });
   });
 }
