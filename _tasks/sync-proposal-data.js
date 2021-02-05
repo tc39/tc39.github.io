17c19ca3640af2eae9fc24d02d9d2a528e260173
Derek Lewis <DerekNonGeneric@inf.is>
2021-02-05 13:36:45 +0000

    fixup! add sync:proposal-data task

--- _tasks/sync-proposal-data.js
+++ _tasks/sync-proposal-data.js
@@ -21,17 +21,10 @@ import sanitizeHtml from 'sanitize-html';
 import unified from 'unified';
 import yaml from 'js-yaml';
 const DIR_DATA = '_data';
-const DIR_TEMP = 'tmp';
 const FILE_PROPOSAL_DATA = pathResolve(DIR_DATA, 'stage3.yml');
-const URL_RAW_PROPOSALS_README = 'https://raw.githubusercontent.com/tc39/proposals/HEAD/README.md';
 const log = consoleLogLevel({ level: 'info' });
-const ghFileImporter = new GhFileImporter({
-    destDir: DIR_TEMP,
-    log: consoleLogLevel({ level: 'debug' }),
-});
-await fsp.rmdir(DIR_TEMP, { recursive: true });
-await fsp.mkdir(DIR_TEMP);
-await ghFileImporter.importFileFromUrl(URL_RAW_PROPOSALS_README);
+const ghFileImporter = new GhFileImporter({ destDir: 'tmp', log: log, });
+const proposalsReadmeMd = await ghFileImporter.fetchFileContents('tc39', 'proposals', 'README.md');
 // -----------------------------------------------------------------------------
 // Events
 // -----------------------------------------------------------------------------
@@ -87,7 +80,7 @@ async function mkCodeTags(markdownSrc) {
  */
 function getUrlFromDoc(linkTextLike) {
     const linkTextLikeRegex = new RegExp(`\\[${linkTextLike}.*\\]: (.*)`, 'g');
-    return linkTextLikeRegex.exec(String(prpslsReadmeMarkdown))[1];
+    return linkTextLikeRegex.exec(String(proposalsReadmeMd))[1];
 }
 /**
  * First checks `stage3.yml` for pre-existing code sample. If present,
@@ -104,43 +97,26 @@ function getUrlFromDoc(linkTextLike) {
 async function getCodeSample(prpslId) {
     const prpslData = yaml.load(readFileSync(FILE_PROPOSAL_DATA, 'utf8'));
     let codeSample = null;
-    if (prpslData) {
-        prpslData.forEach((value) => {
-            // Select the object we want.
-            if (hasOwn(value, 'id') && value.id === prpslId) {
-                if (hasOwn(value, 'example')) {
-                    codeSample = value.example;
-                }
-                else {
-                    codeSample = undefined;
-                }
-            }
-        });
-    }
+    prpslData.forEach(value => {
+        if (hasOwn(value, 'id') && value.id === prpslId)
+            hasOwn(value, 'example') ? codeSample = value.example : codeSample = undefined;
+    });
     // The example was either already filled out manually or simply did not
     // have one (which is fine), so return our discovery early and avoid parsing
     // remote proposal repo READMEs.
-    if (codeSample != null) {
+    if (codeSample != null)
         return codeSample;
-    }
-    // Some proposal repos use a different branch (e.g., `gh-pages`) for their
-    // README file. Because of this, we need repo metadata to find the raw URL.
-    let rawReadmeUrl;
-    const result = await ghFileImporter
-        .fetchPathMetadata('tc39', prpslId, 'README.md');
-    rawReadmeUrl = result.data.download_url;
     // Fetch the file contents and parse out the code samples.
-    const contents = await ghFileImporter.fetchFileContents(rawReadmeUrl);
+    const contents = await ghFileImporter.fetchFileContents('tc39', prpslId, 'README.md');
     const codeBlockRegex = new RegExp(/```[a-z]*\n[\s\S]*?\n```\n/g);
     let codeBlocks = toArray(codeBlockRegex.exec(contents));
     return codeBlocks.length > 0 ?
-        codeBlocks[codeBlocks.length - 1].replace(/```(.?)*?\n+/gm, '') : undefined;
+        codeBlocks.pop()?.replace(/```(.?)*?\n+/gm, '') : undefined;
 }
 function getPrpslId(linkTextLike) {
     let prpslUrl = getUrlFromDoc(linkTextLike);
-    if (prpslUrl.endsWith('/')) {
+    if (prpslUrl.endsWith('/'))
         prpslUrl = prpslUrl.slice(0, -1); // Lose trailing slashes to prevent crash.
-    }
     const prpslId = String(prpslUrl
         ?.split('/')
         ?.pop()
@@ -150,11 +126,10 @@ function getPrpslId(linkTextLike) {
 /** Checks if the proposal has a spec using the files listed by the GitHub API. */
 async function hasSpec(prpslId) {
     let isFound = false;
-    const result = await ghFileImporter.fetchPathMetadata('tc39', prpslId);
+    const result = await ghFileImporter.fetchMetadata('tc39', prpslId);
     result.data.forEach((value) => {
-        if (value.path === 'spec.html') {
+        if (value.path === 'spec.html')
             isFound = true;
-        }
     });
     return isFound;
 }
@@ -170,20 +145,17 @@ function json2yaml(buffer, options) {
  * uses that. If missing, fetches them from the GitHub repo.
  */
 async function getShortDescription(prpslId) {
-    let description = undefined;
+    let description;
     const prpslData = yaml.load(readFileSync(FILE_PROPOSAL_DATA, 'utf8'));
-    prpslData.forEach((value) => {
-        // Select the object we want.
+    prpslData.forEach(value => {
         if (hasOwn(value, 'id') && value.id === prpslId) {
-            if (hasOwn(value, 'description')) {
-                // Proposal description has already has already been filled out, use that.
-                description = value.description;
-            }
+            // Proposal description has already has already been filled out, use that.
+            hasOwn(value, 'description') ? description = value.description : description = undefined;
         }
     });
     if (!description) {
         // The proposal is missing from the dataset, so use the description on GitHub repo.
-        const repoMetadata = await ghFileImporter.fetchRepoMetadata('tc39', prpslId);
+        const repoMetadata = await ghFileImporter.fetchMetadata('tc39', prpslId);
         description = repoMetadata.data.description;
     }
     log.info(`Description for ${curlyQuote(prpslId)}: ${description}`);
@@ -210,15 +182,13 @@ function presenceObjFrom(valObj) {
 // -----------------------------------------------------------------------------
 // Main
 // -----------------------------------------------------------------------------
-const prpslsReadmeMarkdown = await fsp.readFile(pathResolve(DIR_TEMP, 'README.md'));
 const tblRegex = new RegExp(/(### Stage 3\n\n)([.+?\s\S]+)(\n\n### Stage 2)/g);
-const markdownTbl = tblRegex.exec(String(prpslsReadmeMarkdown))[2];
+const markdownTbl = tblRegex.exec(String(proposalsReadmeMd))[2];
 const jsonTbl = mdTbl2json(markdownTbl, (val) => sanitizeHtml(String(val), { allowedTags: ['code', 'br'] }), (val) => sanitizeHtml(String(val).toLowerCase().replace(' ', '_'), { allowedTags: [] }));
 // Now, with our stage 3 table in JSON form, we must take what we need from each
 // row and use the cell contents to construct our ProposalRecord data structure
 // prior to making the JSON -> YAML conversion.
 // -----------------------------------------------------------------------------
-/** @type {Array<Promise<ProposalRecord>>} */
 const prpslRcrdPromiseArr = jsonTbl.map(async (value) => {
     const prpslRcrdId = getPrpslId(getFullRefLinkContent(value.proposal, 1));
     const prpslRcrd = {
@@ -230,20 +200,18 @@ const prpslRcrdPromiseArr = jsonTbl.map(async (value) => {
         has_specification: await hasSpec(prpslRcrdId),
         presented: [presenceObjFrom(value)],
         title: await mkCodeTags(getFullRefLinkContent(value.proposal, 0)),
-        tests: (hasOwn(value, 'tests') && isFullRefLink(value.tests)) ?
+        tests: hasOwn(value, 'tests') && isFullRefLink(value.tests) ?
             [getUrlFromDoc(getFullRefLinkContent(value.tests, 1))] : undefined,
     };
     return prpslRcrd;
 });
 Promise.allSettled(prpslRcrdPromiseArr).then(async (results) => {
     const data = [];
-    results.forEach((result) => {
-        if (result.status === 'fulfilled') {
+    results.forEach(result => {
+        if (result.status === 'fulfilled')
             data.push(result.value);
-        }
-        else {
+        else
             throw new Error(result.reason);
-        }
     });
     const dataBuffer = Buffer.from(JSON.stringify(data));
     const resultBuffer = json2yaml(dataBuffer, {});
--- _tasks/sync-proposal-data.ts
+++ _tasks/sync-proposal-data.ts
@@ -24,21 +24,12 @@ import unified from 'unified';
 import yaml from 'js-yaml';
 
 const DIR_DATA = '_data';
-const DIR_TEMP = 'tmp';
 const FILE_PROPOSAL_DATA = pathResolve(DIR_DATA, 'stage3.yml');
-const URL_RAW_PROPOSALS_README =
-  'https://raw.githubusercontent.com/tc39/proposals/HEAD/README.md';
 
 const log = consoleLogLevel({ level: 'info' });
-
-const ghFileImporter = new GhFileImporter({
-  destDir: DIR_TEMP,
-  log: consoleLogLevel({ level: 'debug' }),
-});
-
-await fsp.rmdir(DIR_TEMP, { recursive: true });
-await fsp.mkdir(DIR_TEMP);
-await ghFileImporter.importFileFromUrl(URL_RAW_PROPOSALS_README);
+const ghFileImporter = new GhFileImporter({ destDir: 'tmp', log: log, });
+const proposalsReadmeMd =
+  await ghFileImporter.fetchFileContents('tc39', 'proposals', 'README.md');
 
 // -----------------------------------------------------------------------------
 // Typedefs
@@ -131,7 +122,7 @@ async function mkCodeTags(markdownSrc:string):Promise<string> {
  */
 function getUrlFromDoc(linkTextLike:string):string {
   const linkTextLikeRegex = new RegExp(`\\[${linkTextLike}.*\\]: (.*)`, 'g');
-  return linkTextLikeRegex.exec(String(prpslsReadmeMarkdown))![1];
+  return linkTextLikeRegex.exec(String(proposalsReadmeMd))![1];
 }
 
 /**
@@ -152,46 +143,29 @@ async function getCodeSample(prpslId:string):Promise<(string | undefined)> {
 
   let codeSample = null;
 
-  if (prpslData) {
-    prpslData.forEach((value) => {
-      // Select the object we want.
-      if (hasOwn(value, 'id') && value.id === prpslId) {
-        if (hasOwn(value, 'example')) {
-          codeSample = value.example;
-        } else {
-          codeSample = undefined;
-        }
-      }
-    });
-  }
- 
+  prpslData.forEach(value => {
+    if (hasOwn(value, 'id') && value.id === prpslId)
+      hasOwn(value, 'example') ? codeSample = value.example : codeSample = undefined
+    }
+  );
+
   // The example was either already filled out manually or simply did not
   // have one (which is fine), so return our discovery early and avoid parsing
   // remote proposal repo READMEs.
-  if (codeSample != null) {
-    return codeSample;
-  }
-
-  // Some proposal repos use a different branch (e.g., `gh-pages`) for their
-  // README file. Because of this, we need repo metadata to find the raw URL.
-  let rawReadmeUrl;
-  const result:any = await ghFileImporter
-    .fetchPathMetadata('tc39', prpslId, 'README.md');
-  rawReadmeUrl = result.data.download_url;
+  if (codeSample != null) return codeSample;
 
   // Fetch the file contents and parse out the code samples.
-  const contents = await ghFileImporter.fetchFileContents(rawReadmeUrl);
+  const contents = await ghFileImporter.fetchFileContents('tc39', prpslId, 'README.md');
   const codeBlockRegex = new RegExp(/```[a-z]*\n[\s\S]*?\n```\n/g);
   let codeBlocks:string[] = toArray(codeBlockRegex.exec(contents));
   return codeBlocks.length > 0 ?
-    codeBlocks[codeBlocks.length - 1].replace(/```(.?)*?\n+/gm, '') : undefined;
+    codeBlocks.pop()?.replace(/```(.?)*?\n+/gm, '') : undefined;
 }
 
 function getPrpslId(linkTextLike:string):string {
   let prpslUrl:string = getUrlFromDoc(linkTextLike);
-  if (prpslUrl.endsWith('/')) {
+  if (prpslUrl.endsWith('/'))
     prpslUrl = prpslUrl.slice(0, -1); // Lose trailing slashes to prevent crash.
-  }
 
   const prpslId = String(prpslUrl
     ?.split('/')
@@ -204,11 +178,9 @@ function getPrpslId(linkTextLike:string):string {
 /** Checks if the proposal has a spec using the files listed by the GitHub API. */
 async function hasSpec(prpslId:string):Promise<boolean> {
   let isFound = false;
-  const result:any = await ghFileImporter.fetchPathMetadata('tc39', prpslId);  
+  const result:any = await ghFileImporter.fetchMetadata('tc39', prpslId);  
   result.data.forEach((value:any) => {
-    if (value.path === 'spec.html') {
-      isFound = true;
-    }
+    if (value.path === 'spec.html') isFound = true;
   });
   return isFound;
 }
@@ -226,22 +198,20 @@ function json2yaml(buffer:Uint8Array, options:yaml.DumpOptions):Uint8Array {
  * uses that. If missing, fetches them from the GitHub repo.
  */
 async function getShortDescription(prpslId:string):Promise<(string | undefined)> {
-  let description = undefined;
+  let description;
   const prpslData:Array<ProposalRecord>
     = yaml.load(readFileSync(FILE_PROPOSAL_DATA, 'utf8')) as Array<ProposalRecord>;
-  prpslData.forEach((value) => {
-    // Select the object we want.
+
+  prpslData.forEach(value => {
     if (hasOwn(value, 'id') && value.id === prpslId) {
-      if (hasOwn(value, 'description')) {
-        // Proposal description has already has already been filled out, use that.
-        description = value.description;
-      }
+      // Proposal description has already has already been filled out, use that.
+      hasOwn(value, 'description') ? description = value.description : description = undefined
     }
   });
 
   if (!description) {
     // The proposal is missing from the dataset, so use the description on GitHub repo.
-    const repoMetadata:any = await ghFileImporter.fetchRepoMetadata('tc39', prpslId);
+    const repoMetadata:any = await ghFileImporter.fetchMetadata('tc39', prpslId);
     description = repoMetadata.data.description;
   }
 
@@ -272,9 +242,8 @@ function presenceObjFrom(valObj:ProposalStageTableRecord):PresenceObj {
 // Main
 // -----------------------------------------------------------------------------
 
-const prpslsReadmeMarkdown = await fsp.readFile(pathResolve(DIR_TEMP, 'README.md'));
 const tblRegex = new RegExp(/(### Stage 3\n\n)([.+?\s\S]+)(\n\n### Stage 2)/g);
-const markdownTbl = tblRegex.exec(String(prpslsReadmeMarkdown))![2];
+const markdownTbl = tblRegex.exec(String(proposalsReadmeMd))![2];
 const jsonTbl:ProposalStageTableRecord[] =
   mdTbl2json(markdownTbl,
     (val:string) => sanitizeHtml(String(val), { allowedTags: ['code', 'br'] }),
@@ -286,8 +255,8 @@ const jsonTbl:ProposalStageTableRecord[] =
 // prior to making the JSON -> YAML conversion.
 // -----------------------------------------------------------------------------
 
-/** @type {Array<Promise<ProposalRecord>>} */
-const prpslRcrdPromiseArr = jsonTbl.map(async (value) => {
+const prpslRcrdPromiseArr:Array<Promise<ProposalRecord>>
+  = jsonTbl.map(async (value) => {
   const prpslRcrdId = getPrpslId(getFullRefLinkContent(value.proposal, 1));
   const prpslRcrd:ProposalRecord = {
     id: prpslRcrdId,
@@ -298,20 +267,17 @@ const prpslRcrdPromiseArr = jsonTbl.map(async (value) => {
     has_specification: await hasSpec(prpslRcrdId),
     presented: [presenceObjFrom(value)],
     title: await mkCodeTags(getFullRefLinkContent(value.proposal, 0)),
-    tests: (hasOwn(value, 'tests') && isFullRefLink(value.tests)) ?
+    tests: hasOwn(value, 'tests') && isFullRefLink(value.tests) ?
       [getUrlFromDoc(getFullRefLinkContent(value.tests, 1))] : undefined,
   }
   return prpslRcrd;
 });
 
-Promise.allSettled(prpslRcrdPromiseArr).then(async (results) => {
+Promise.allSettled(prpslRcrdPromiseArr).then(async results => {
   const data:ProposalRecord[] = [];
-  results.forEach((result) => {
-    if (result.status === 'fulfilled') {
-      data.push(result.value);
-    } else {
-      throw new Error(result.reason);
-    }    
+  results.forEach(result => {
+    if (result.status === 'fulfilled') data.push(result.value)
+    else throw new Error(result.reason)
   });
   const dataBuffer:Uint8Array = Buffer.from(JSON.stringify(data));
   const resultBuffer:Uint8Array = json2yaml(dataBuffer, {});
