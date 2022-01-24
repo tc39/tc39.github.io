2ccc28dc4116710dbe7d135e6e41158b70fd663a
Jason Williams <jwilliams720@bloomberg.net>
2022-01-24 11:58:24 +0000

    octicons have been added back in via page data.

---
+++ _data/octicons.js
@@ -0,0 +1,7 @@
+const octicons = require('@primer/octicons');
+
+// @ts-check
+module.exports = {
+  commentDiscussion: octicons['comment-discussion'].heights[16].path,
+  markGithub: octicons['mark-github'].heights[16].path,
+};
