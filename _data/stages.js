4fc0f2e240be6df460b5e0c59b5cf684a23407f2
jasonwilliams <jase.williams@gmail.com>
2022-07-29 12:44:26 +0100

    Power proposals from Dataset API
    - Add back editorConfig, its useful when commiting from Windows to make sure LF is set.
    - eleventyApi uses a .cache file to store the response ignore this.
    
    Needed from dataset..
    - Presented?
    - Presented Date
    - Has specification?
    - examples
    - ECMA-262 doesn't need adding to the tags if tests is already available as a property

---
+++ _data/stages.js
@@ -0,0 +1,25 @@
+const EleventyFetch = require('@11ty/eleventy-fetch');
+
+function arrayEquals(a, b) {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+}
+
+module.exports = async function () {
+  let json = await EleventyFetch('https://tc39.es/dataset/proposals.json', {
+    duration: '1h', // 1 day
+    type: 'json', // also supports "text" or "buffer"
+  });
+
+  const stage3 = json.filter((v) => v.stage === 3);
+  return stage3.map((v) => {
+    v.authorsAndChampions = arrayEquals(v.authors, v.champions)
+      ? v.authors
+      : false;
+    return v;
+  });
+};
